0f6cd374440999ddf4453197a7327e5f16d84bee
HuiiKuo <11136008@ntub.edu.tw>
2023-02-13 00:29:31 +0900

    addpeople api

--- model/peopleModel.py
+++ model/peopleModel.py
@@ -5,5 +5,8 @@ from model.db import mongo
 # name,gender,birth,height,weight
 
 def getpeople():
-    return list(mongo.db.people.find())
+    return list(mongo.db.people.find({},{"_id":0}))
+
+def addpeople(name,gender,birth,height,weight):
+    return list(mongo.db.people.insert_one({"name":name,"gender":gender,"birth":birth,"height":height,"weight":weight}))
 
